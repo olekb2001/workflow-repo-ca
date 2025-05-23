78d2d1d6266f4ad106c948f2bb41218ceaab0cc1
olekb2001 <olekb2001@gmail.com>
2025-05-23 21:37:13 +0200

    fixed the playwright config

--- playwright.config.js
+++ playwright.config.js
@@ -1,8 +1,11 @@
 // playwright.config.js
-const { defineConfig } = require('@playwright/test');
-require('dotenv').config();
+import { defineConfig } from '@playwright/test';
+import dotenv from 'dotenv';
 
-module.exports = defineConfig({
+dotenv.config();
+
+export default defineConfig({
+  testDir: './tests',
   use: {
     baseURL: process.env.BASE_URL || 'http://127.0.0.1:5500/',
   },
