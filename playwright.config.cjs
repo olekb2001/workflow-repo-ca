8940a0501da645090360eca29dd585fa630dee1f
olekb2001 <olekb2001@gmail.com>
2025-05-23 22:16:27 +0200

    test

--- playwright.config.cjs
+++ playwright.config.cjs
@@ -4,6 +4,8 @@ require('dotenv').config();
 module.exports = defineConfig({
   testDir: './tests',
   use: {
-    baseURL: process.env.BASE_URL || 'http://127.0.0.1:8080', 
+    baseURL: process.env.BASE_URL || 'http://127.0.0.1:8080',
+    headless: false,       // so browser UI shows up
+    slowMo: 6000,          // slows down actions by 1000ms (1 second)
   },
 });
