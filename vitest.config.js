9a7acba3325746384638791273ab737e3a69c63b
olekb2001 <olekb2001@gmail.com>
2025-05-23 22:38:05 +0200

    fix

--- vitest.config.js
+++ vitest.config.js
@@ -4,6 +4,9 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    exclude: ['tests/**/*.spec.js'], // exclude Playwright tests
+    exclude: [
+      'tests/**/*.spec.js', // exclude your Playwright tests
+      'node_modules/**', // exclude all node_modules tests
+    ],
   },
 });
