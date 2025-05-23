68a2a5f8b8a9c17a5ed64d6a83af89d2c8bcdba2
olekb2001 <olekb2001@gmail.com>
2025-05-23 21:38:51 +0200

    error fix

---
+++ playwright.config.cjs
@@ -0,0 +1,10 @@
+// playwright.config.cjs
+const { defineConfig } = require('@playwright/test');
+require('dotenv').config();
+
+module.exports = defineConfig({
+  testDir: './tests', 
+  use: {
+    baseURL: process.env.BASE_URL || 'http://127.0.0.1:5500/',
+  },
+});
