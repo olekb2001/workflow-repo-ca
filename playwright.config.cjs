b8f5785cf70b7ee5dfae5200974f73ef03feb939
olekb2001 <olekb2001@gmail.com>
2025-05-23 22:57:45 +0200

    final commit

--- playwright.config.cjs
+++ playwright.config.cjs
@@ -6,6 +6,6 @@ module.exports = defineConfig({
   use: {
     baseURL: process.env.BASE_URL || 'http://127.0.0.1:8080',
     headless: false,       // so browser UI shows up
-    slowMo: 6000,          // slows down actions by 1000ms (1 second)
+    slowMo: 6000,          // slows down actions by 6000ms
   },
 });
